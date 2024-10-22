fd8431afa472678886e0a0c0609750a36383c773
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-10-22 22:08:12 +0600

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <main className="flex-1 overflow-y-auto overflow-x-hidden">
+        <Header />
+      </main>
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import MenuSVG from "../../SVGs/MenuSVG";
+import NotificationSVG from "../../SVGs/NotificationSVG";
+import HeaderMessageSVG from "../../SVGs/HeaderMessageSVG";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between bg-gray-800 p-4">
+      <button className="lg:hidden">
+        <MenuSVG />
+      </button>
+      <div className="mx-4 flex-1">
+        <input
+          type="text"
+          placeholder="Search here"
+          className="w-full max-w-xl rounded-full bg-gray-700 px-4 py-2 text-white focus:outline-none"
+        />
+      </div>
+      <div className="flex items-center">
+        <button className="relative mr-4">
+          <NotificationSVG />
+          <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500"></span>
+        </button>
+        <button className="relative mr-4">
+          <HeaderMessageSVG />
+          <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500"></span>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
