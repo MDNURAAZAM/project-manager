34d664c0e8ed21c1954723e6b924deec07f7bc79
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-10-25 11:46:24 +0600

    change title of revise to revised

--- src/components/RevisedContainer/RevisedContainer.jsx
+++ src/components/RevisedContainer/RevisedContainer.jsx
@@ -11,7 +11,7 @@ const RevisedContainer = ({ tasks }) => {
     <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
       <div className="rounded-lg bg-rose-500 p-4">
         <div className="mb-2 flex items-center justify-between">
-          <h3 className="text-lg font-semibold">Revise ({tasks?.length})</h3>
+          <h3 className="text-lg font-semibold">Revised ({tasks?.length})</h3>
           {tasks?.length > 0 && (
             <button onClick={() => setIsAscending((prev) => !prev)}>
               <SortSVG />
@@ -24,7 +24,7 @@ const RevisedContainer = ({ tasks }) => {
               <RevisedItem key={task.id} task={task} />
             ))
           ) : (
-            <p>Revise List is empty</p>
+            <p>Revised List is empty</p>
           )}
         </div>
       </div>
